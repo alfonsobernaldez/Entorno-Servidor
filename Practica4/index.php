da2ac1ff75b0317adbc4b1ca130fd703847415b4
alfonsobernaldez <46372067+alfonsobernaldez@users.noreply.github.com>
2022-05-16 07:12:45 +0200

    Practica 4

---
+++ Practica4/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <h2>Datos del alumno</h2>
+<input name="dni">Introduzca el dni del alumno
+<input name="apellido"> Introduzca el apellido
+<input type="submit">
+
+</form>
+
+    
+</body>
+</html>
